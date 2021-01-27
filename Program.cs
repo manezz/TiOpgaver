8be41c0d0cfa42e73bafb0d51a3979bf84ee59b0
Magnus <68356591+manezz@users.noreply.github.com>
2021-01-27 12:11:22 +0100

    opgave 1 ferdig

--- Program.cs
+++ Program.cs
@@ -9,19 +9,26 @@ class Program
 		 {
 			 while (true)
 			 {
-				 int caseSwitch = 0;
+				 int caseSwitch = 1;
 				 switch (caseSwitch)
 				 {
 					 case 1:
 					 Matix_10x10();
 					 break;
 				 }
-				 
+
 				static void Matix_10x10()
 				{
-
+					for (int i = 1; i <= 11; i++)
+					{
+						System.Console.WriteLine();
+						for (int u = 1; u <= 10; u++)
+						{
+							System.Console.Write($" {i * u}");
+						}
+					}	
 				}
-
+				break;
 			 }
 		 }
 	 }
