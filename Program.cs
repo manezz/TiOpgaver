d614ca7efc7238c35491a57dfe61cc0d3336082d
Magnus <68356591+manezz@users.noreply.github.com>
2021-01-27 11:45:51 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TiOpgaver
+{
+class Program
+	 {
+		 static void Main(string[] args)
+		 {
+			 while (true)
+			 {
+				 int caseSwitch = 0;
+				 switch (caseSwitch)
+				 {
+					 case 1:
+					 Matix_10x10();
+					 break;
+				 }
+				 
+				static void Matix_10x10()
+				{
+
+				}
+
+			 }
+		 }
+	 }
+}
